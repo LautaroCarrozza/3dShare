64ddf00a6d5fc974d74196d7511ce8253268c3ce
Lautaro Carrozza <lautaro.carrozza@ing.austral.edu.ar>
2019-03-27 11:17:46 -0300

    Printer entity added

---
+++ src/main/java/lab1/dShare/D_Share/PrinterModel/PrinterRepository.java
@@ -0,0 +1,7 @@
+package lab1.dShare.D_Share.PrinterModel;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface PrinterRepository extends CrudRepository<Printer, String> {
+}
